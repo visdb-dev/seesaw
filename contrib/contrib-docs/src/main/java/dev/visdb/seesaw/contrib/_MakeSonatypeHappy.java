e7f50f3c917c842074b7a32116742e66f7096228
Ernie Rael <errael@raelity.com>
2026-09-16 16:06:18 -0700

    Javadoc for contrib.
    Change javadoc-publish.yml to do javadoc:aggregate, adjust path.
    
    New module contrib-docs, package dev.visdb.seesaw.contrib. Only contains
    package-info.java. The javadoc page automatically contains links to the
    contrib packages.

---
+++ contrib/contrib-docs/src/main/java/dev/visdb/seesaw/contrib/_MakeSonatypeHappy.java
@@ -0,0 +1,13 @@
+package dev.visdb.seesaw.contrib;
+
+/**
+ * This class exists to generate empty javadoc and source jars to
+ * satisfy Sonatype's staging repository.  This project is simply
+ * package-info.java.
+ */
+public final class _MakeSonatypeHappy {
+
+    private _MakeSonatypeHappy() {
+        throw new AssertionError();
+    }
+}
---
+++ contrib/contrib-docs/src/main/java/dev/visdb/seesaw/contrib/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * Generally contrib projects/packages are individual maven artifacts
+ * where each artifact is based on 3rd-party dependencies.
+ */
+package dev.visdb.seesaw.contrib;
